3219eb371556f0ebee5890678ff4626ba3a39a7a
Emily <aspentree79@gmail.com>
2019-07-18 01:02:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,9 +26,9 @@ def using_concat(arr1, arr2)
   arr1.concat(arr2)
 end
 
-# def using_insert(array, element)
-#   array.insert(3, element)
-# end
+def using_insert(array, element)
+  array.insert(4, element)
+end
 
 def using_uniq(array)
 array.uniq
